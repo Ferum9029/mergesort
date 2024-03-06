6619bd2b33a0fa210ddf26d48b5ef6f7523a8a99
Ferum <ferum9029@gmail.com>
2024-03-06 15:25:11 +0300

    funny line

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sorting
 import iostream
 
 if __name__ == "__main__":
-    print(sorting.merge_sort([2, 11, 13, 5, 6, 7]))
-    print(sorting.merge_sort([2, 11, 13, 5, 6, 7, 23]))
-    iostream.console_output(sorting.merge_sort(iostream.file_input()))
-
+    iostream.console_output(sorting.merge_sort([2, 11, 13, 5, 6, 7]))
+    iostream.console_output(sorting.merge_sort([2, 11, 13, 5, 6, 7, 23]))
+    iostream.console_output(sorting.merge_sort((lambda x: print(x) or x)(iostream.random_input(15))))
